d6c6ec180fb228e5e179599dd9fcb6e8e98634e5
dgzamo <dayisleguizamo@hotmail.com>
2022-10-28 12:48:14 -0500

    Crud/Registrar Productos

--- src/App.js
+++ src/App.js
@@ -8,15 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Bienvenido</p>
-        <buttom variant="contained" color="success">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ingresar 
-        </a></buttom>
+        
       </header>
     </div>
   );
